82de4c144697bbb4552f7f8a5e18fa5b04538e8a
shireeshavidem <shireesha.marolix@gmail.com>
2023-10-16 17:46:45 +0530

    fifth commit

--- 13-10funcTask.py
+++ 13-10funcTask.py
@@ -55,29 +55,39 @@
 
 # 4.Write a Python function to create and print a list where the values are the squares of numbers between 1 and 10 (both included). 
 
-def printSquares():
-	sqrs = list()
-	for i in range(1,11):
-		sqrs.append(i**2)
-	print(sqrs)
-printSquares()
+# def printSquares():
+# 	sqrs = list()
+# 	for i in range(1,11):
+# 		sqrs.append(i**2)
+# 	print(sqrs)
+# printSquares()
 
 # 5.Write a Python function to sum all the numbers in a list.
 # Sample List : (8, 2, 3, 0, 7)
 # Expected Output : 20 
 
-n = int(input("enter no of elements: "))
-def sumValue():
-    list = []
-    sum = 0
-    for i in range(0,n):
-        list1 = int(input("Enter list elements: "))
-        list.append(list1)
-    for i in range(len(list)):
-        sum = sum + list[i]
-    print(list)
-    print(sum)    
-sumValue()
+# n = int(input("enter no of elements: "))
+# def sumValue():
+#     list = []
+#     sum = 0
+#     for i in range(0,n):
+#         list1 = int(input("Enter list elements: "))
+#         list.append(list1)
+#     for i in range(len(list)):
+#         sum = sum + list[i]
+#     print(list)
+#     print(sum)    
+# sumValue()
 
 # 6.write a function to find sum of given values, pass values has variable number of arguments to function.
-
+def add_num(*args):
+   sum = 0
+   for num in args:
+      sum += num
+   return sum
+add_num(1, 2, 3)
+print(add_num(1, 2, 3))
+add_num(10, 20, 30, 40)
+print(add_num(10, 20, 30, 40))
+add_num(5, 6, 7, 8, 9)
+print(add_num(5, 6, 7, 8, 9))
